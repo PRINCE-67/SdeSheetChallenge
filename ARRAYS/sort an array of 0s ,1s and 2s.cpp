407146e756327e58df43443498e06222768a8452
Prince Verma <princeverma694269@gmail.com>
2022-06-06 16:01:28 +0530

    sort 0 1 2 in array

---
+++ ARRAYS/sort an array of 0s ,1s and 2s.cpp
@@ -0,0 +1,30 @@
+//------------------------tc O(2n)--------------------------------
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) cnt0++;
+            if (nums[i] == 1) cnt1++;
+            if (nums[i] == 2) cnt2++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (cnt0 > 0) {
+                nums[i] = 0;
+                cnt0--;
+            }
+            else if (cnt1 > 0) {
+                nums[i] = 1;
+                cnt1--;
+            }
+            else if (cnt2 > 0) {
+                nums[i] = 2;
+                cnt2--;
+            }
+        }
+    }
+};
