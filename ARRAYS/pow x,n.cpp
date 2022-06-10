14eb71a4e4b38d17c660f1411be39e1e138e29ad
Prince Verma <princeverma694269@gmail.com>
2022-06-10 18:38:11 +0530

    pow (x,n)

---
+++ ARRAYS/pow x,n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1;
+        long long nn = n;
+        if (n < 0) nn = nn * -1;
+        while (nn > 0) {
+            if (nn % 2 == 0) {
+                x = x * x;
+                nn /= 2;
+            }
+            else {
+                ans = ans * x;
+                nn -= 1;
+            }
+        }
+        if (n < 0) ans = 1.0 / ans;
+        return ans;
+    }
+};
