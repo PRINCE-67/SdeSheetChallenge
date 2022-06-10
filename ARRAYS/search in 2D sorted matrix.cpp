2f08530e301d3d32680b6ea398f6bd026cefdbe7
Prince Verma <princeverma694269@gmail.com>
2022-06-10 16:15:29 +0530

    search in 2d sorted matrix

---
+++ ARRAYS/search in 2D sorted matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int l = 0;
+        int h = (m * n) - 1;
+
+        while (l <= h) {
+            int mid = (l + h) / 2;
+
+            if (matrix[mid / n][mid % n] == target) return true;
+
+            if (matrix[mid / n][mid % n] < target) l = mid + 1;
+            else h = mid - 1;
+        }
+        return false;
+    }
+};
